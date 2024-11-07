a729e5f22b1cf13fa39b194d92b30831d42021f3
Garrett <gat2835@rit.edu>
2024-11-07 16:58:05 -0500

    fixed formatting

--- main.go
+++ main.go
@@ -21,11 +21,11 @@ func helloWorlHtml(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprint(w, "Hello World")
 	switch r.URL.Path {
 	case "/":
-		fmt.Fprint(w, "</h1>Hello World</h1>")
+		fmt.Fprint(w, "<h1>Hello World</h1>")
 	case "/admin":
-		fmt.Fprint(w, "</h1>I hate team18</h1>")
+		fmt.Fprint(w, "<h1>I hate team18</h1>")
 	default:
-		fmt.Fprint(w, "</h1>404 File not found</h1>")
+		fmt.Fprint(w, "<h1>404 File not found</h1>")
 	}
 }
 
