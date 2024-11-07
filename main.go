fbefd97723afc2c15151dbd27e974cb7a9477574
Garrett <gat2835@rit.edu>
2024-11-07 16:56:44 -0500

    added html formatting

--- main.go
+++ main.go
@@ -15,11 +15,22 @@ func helloWorldPage(w http.ResponseWriter, r *http.Request) {
 	default:
 		fmt.Fprint(w, "404 File not found")
 	}
+}
 
+func helloWorlHtml(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, "Hello World")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "</h1>Hello World</h1>")
+	case "/admin":
+		fmt.Fprint(w, "</h1>I hate team18</h1>")
+	default:
+		fmt.Fprint(w, "</h1>404 File not found</h1>")
+	}
 }
 
 func main() {
-	http.HandleFunc("/", helloWorldPage)
+	http.HandleFunc("/", helloWorlHtml)
 	fmt.Println("Listening on port 80")
 	err := http.ListenAndServe("", nil)
 	if err != nil {
